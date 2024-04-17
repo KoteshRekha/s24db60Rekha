4af0553b6cf3d3f76ea8e057fd8940637d58fd13
KoteshRekha <156227398+KoteshRekha@users.noreply.github.com>
2024-04-16 21:49:40 -0500

    1

--- models/jacket.js
+++ models/jacket.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const jacketSchema = mongoose.Schema({
 jacket_name: String,
-cloth: String,
-cost: Number
+cloth: {
+    type: String,
+    minlength: 1,
+    maxlength: 15,
+},
+cost: {
+    type: Number,
+    min: 1,
+    max: 500,
+
+}
 })
 module.exports = mongoose.model("jacket",
 jacketSchema)
--- routes/jacket.js
+++ routes/jacket.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const jacket_controlers= require('../controllers/jacket');
 var router = express.Router();
 /* GET jacket */
@@ -8,8 +9,23 @@ router.get('/detail', jacket_controlers.jacket_view_one_Page);
 /* GET create jacket page */
 router.get('/create', jacket_controlers.jacket_create_Page);
 /* GET update jacket page */
-router.get('/update', jacket_controlers.jacket_update_Page);
+//router.get('/update', jacket_controlers.jacket_update_Page);
 /* GET delete jacket page */
-router.get('/delete', jacket_controlers.jacket_delete_Page);
+//router.get('/delete', jacket_controlers.jacket_delete_Page);
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/update',secured, jacket_controlers.jacket_update_Page);
+router.get('/delete',secured, jacket_controlers.jacket_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    
 module.exports = router;
 
